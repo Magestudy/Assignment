096fe59316db4d49a82bf77a7d0d6b0b5ae7c0b2
Shiwani <shiwani27miglani@gmail.com>
2022-05-30 18:17:24 +0930

    adding assignment

---
+++ app/code/Testing/Assignment/Block/Form.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Testing\Assignment\Block;
+
+class Form extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template $context,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+       }
+
+    /**
+     * Get form action URL for POST booking request
+     *
+     * @return string
+     */
+    public function getFormAction()
+    {
+            // companymodule is given in routes.xml
+            // controller_name is folder name inside controller folder
+            // action is php file name inside above controller_name folder
+
+        return '/testingassignment/index/form';
+        // here controller_name is index
+    }
+}
---
+++ app/code/Testing/Assignment/Controller/Index/Form.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Testing\Assignment\Controller\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Config\Storage\WriterInterface;
+class Form extends \Magento\Framework\App\Action\Action
+{
+    protected $resultPageFactory;
+	
+	 public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+		WriterInterface $configWriter
+    ) {
+        $this->resultPageFactory = $resultPageFactory;        
+        $this->_scopeConfig = $scopeConfig;
+		$this->configWriter = $configWriter;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        
+        $post = (array) $this->getRequest()->getPost(); //get post request for form data
+		if (!empty($post)) {
+            // Retrieve form data
+            $livechat_license_number   = $post['livechat_license_number'];
+            $livechat_groups           = $post['livechat_groups'];
+            $livechat_params           = $post['livechat_params'];
+			$this->configWriter->save('livechat/general/license', $livechat_license_number);
+			$this->configWriter->save('livechat/advanced/group',  $livechat_groups);
+			$this->configWriter->save('livechat/advanced/params', $livechat_params);
+			
+            $this->messageManager->addSuccessMessage('Form Submitted!!');
+
+            // Redirect to form page 
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('assignment/index/form'); 
+
+            return $resultRedirect;
+        }
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    } 
+}
