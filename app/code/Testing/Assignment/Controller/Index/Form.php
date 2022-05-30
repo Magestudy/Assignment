6aa85caa2223f63913b911265610df4bab200156
Shiwani <shiwani27miglani@gmail.com>
2022-05-30 19:03:31 +0930

    add description in redme file

--- app/code/Testing/Assignment/Controller/Index/Form.php
+++ app/code/Testing/Assignment/Controller/Index/Form.php
@@ -6,10 +6,20 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 class Form extends \Magento\Framework\App\Action\Action
 {
+	/**
+	* @var resultPageFactory
+	*/
     protected $resultPageFactory;
-	
+	/**
+     * InfoBox constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param ScopeConfigInterface $scopeConfig
+	 * @param WriterInterface $configWriter
+     */
 	 public function __construct(
-        \Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
 		WriterInterface $configWriter
@@ -29,10 +39,11 @@ class Form extends \Magento\Framework\App\Action\Action
             $livechat_license_number   = $post['livechat_license_number'];
             $livechat_groups           = $post['livechat_groups'];
             $livechat_params           = $post['livechat_params'];
-			$this->configWriter->save('livechat/general/license', $livechat_license_number);
+			// Save fields via configWriter
+			$this->configWriter->save('livechat/general/license', $livechat_license_number); 
 			$this->configWriter->save('livechat/advanced/group',  $livechat_groups);
 			$this->configWriter->save('livechat/advanced/params', $livechat_params);
-			
+			// To display the submission message
             $this->messageManager->addSuccessMessage('Form Submitted!!');
 
             // Redirect to form page 
